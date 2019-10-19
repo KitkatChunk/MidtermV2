b14ad584a7269eda6a14be1faeea9fc6ca2f1eca
Kareem <karemalani2402@yahoo.com>
2019-10-19 16:54:06 -0400

    created scene 2

---
+++ AbdulkaremAlani_Midterm_V2/Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Abdulkarem Alani #300993768
+/// This is a background controller that moves and resets the background from right to left. 
+/// </summary>
+
+public class BackgroundController : MonoBehaviour
+{
+
+    public float horizontalSpeed = 0.5f;
+    public float resetPosition = 4.8f;
+    public float resetPoint = -5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (transform.position.x <= resetPoint)
+        {
+            Reset();
+        }
+        
+    }
+
+    // Moving the background to the left, using horizontal speed
+    void Move()
+    {
+        Vector2 newPosition = new Vector2(horizontalSpeed, 0f);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+
+    
+    // This method resets the background to the resetPosition
+    void Reset()
+    {
+        transform.position = new Vector2(resetPosition, 0f );
+
+    }
+
+
+
+
+}
---
+++ AbdulkaremAlani_Midterm_V2/Assets/_Scripts/Obsticle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Abdulkarem Alani #300993768
+/// This moves the enemy Spaceship UFO from right to left and resets them .
+/// </summary>
+
+public class Obsticle : MonoBehaviour
+{
+    public float horizontalSpeed = 0.05f;
+    public float resetPosition = 5f;
+    public float resetPoint = -5f;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if (transform.position.x <= resetPoint)
+        {
+            Reset();
+        }
+    }
+
+    
+    // Moving the Spaceship using certain horizontal speed (from left to right)
+   
+    void Move()
+    {
+        Vector2 newPosition = new Vector2(horizontalSpeed, 0.0f);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+
+   // This method resets the Spaceship to start position.
+    void Reset()
+    {
+        float randomYPosition = Random.RandomRange(4.5f, -4.5f);
+        transform.position = new Vector2(resetPosition, randomYPosition);
+    }
+}
+
